ac3ab5ab089b2aaab6041f4536df067655116029
Agrim Jaimini <agrim.jaimini29@gmail.com>
2025-01-05 18:51:02 -0600

    websocket handling

--- src/static/script.js
+++ src/static/script.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://" + window.location.hostname + ':' + location.port);
+var socket = io.connect("https://" + window.location.hostname + ':' + location.port);
 
 socket.on('path_update', function(data) {
     var pathLinks = data.path.map(function(page, index) {
